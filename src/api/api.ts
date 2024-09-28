7fb55f4b57a1749a1ddff896f2e54bf7e784e8c6
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-09-28 21:40:34 +0300

    add type quoteApi func

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 const instance = 'https://api.quotable.io'
 
 export const quoteAPI = {
-    async getRandomQuote(setQoute, setCopy) {
+    async getRandomQuote(setQoute: React.Dispatch<React.SetStateAction<{ content: string; author: string } | null>>, setCopy: React.Dispatch<React.SetStateAction<string | null>>) {
         const response = await fetch(`${instance}/random`)
         const data = await response.json()
         setQoute(data)
