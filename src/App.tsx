530dd42b35a103c872c22d9a622e6cca91ba7880
ArtemRubtsov <ua.rubtsov.a@gnail.com>
2024-10-01 23:55:01 +0300

    delete custom component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Container } from './components/Container/Container'
 import { Quote } from './components/Quote/Quote';
 import './i18n';
-import { Input } from '@rubtsov/ui-components-react';
 import '@rubtsov/ui-components-react/dist/style.css'
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
 
   return (
     <Container>
-      <Input variant='default' />
       <Quote />
     </Container>
   )
